afd09cc0d70747d346bc87a8e7d6daba82e772c5
tomideo1 <ayotomideaina@gmail.com>
2019-03-29 15:03:53 +0100

    update

---
+++ database/migrations/2019_03_29_150003_create_data_usage.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataUsage extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_usage', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('bytes_sent');
+            $table->bigInteger('bytes_received');
+            $table->string('computer_name');
+            $table->string('logged_on_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_usage');
+    }
+}
